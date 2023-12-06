62c003b2076a0f79d59c121b546f6110c091500a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-06 15:33:27 -0700

    Functions Breakdown in C++

--- Chapter#2:DivingIn/StatementsAndFunctions/OverviewExampleSF.cpp
+++ Chapter#2:DivingIn/StatementsAndFunctions/OverviewExampleSF.cpp
@@ -13,3 +13,16 @@ int main(int argc, char **argv) {
     return 0;
 
 }
+
+// MARK: - Notes:
+
+    // Statements:
+        // Statements are executed in order from top to bottom when the program is run
+        // Execution keeps going until there is a statement causing the program to terminate,
+        //  or run another sequence of statements
+
+
+// MARK: - Concept of Functions
+
+    // Functions:
+        // You give it input and it gives you output
