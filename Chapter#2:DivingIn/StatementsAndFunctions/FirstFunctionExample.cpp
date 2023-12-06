b5bb5ade6c28d167e10c626359d9a0e9f32ec280
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-06 15:41:49 -0700

    more function examples

--- Chapter#2:DivingIn/StatementsAndFunctions/FirstFunctionExample.cpp
+++ Chapter#2:DivingIn/StatementsAndFunctions/FirstFunctionExample.cpp
@@ -2,9 +2,27 @@
 
 #include <iostream>
 
-int main() {
+// Function must be defined before you call it
 
-    
+int addNumbers(int first_number, int second_number) {
+    int sum = first_number + second_number;
+    return sum;
+}
+
+
+int main(int argc, char **argv) {
+
+    int firstNumber = 12;
+    int secondNumber = 9;
+
+    int sum = firstNumber + secondNumber;
+
+    sum = addNumbers(firstNumber, secondNumber);
+
+    sum = addNumbers(34,7);
+
+    std::cout << "The sum of the two numbers is : " << sum << std::endl;
+    std::cout << "The sum of the two numbers is : " << addNumbers(23,8) << std::endl;
 
     return 0;
 
---
+++ Chapter#2:DivingIn/StatementsAndFunctions/FunctionsExample2.cpp
@@ -0,0 +1,11 @@
+
+
+#include <iostream>
+
+int main() {
+
+    int first_number {3}; // Statement
+
+    return 0;
+    
+}
