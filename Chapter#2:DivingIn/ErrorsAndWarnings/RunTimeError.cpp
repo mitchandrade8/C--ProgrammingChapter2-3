0e98925c4be74b89db80d3c802cd2651a8a43e4b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-06 14:37:05 -0700

    Start: of Statements and Functions in C++

---
+++ Chapter#2:DivingIn/ErrorsAndWarnings/RunTimeError.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+
+
+int main() {
+
+    // Run Time Error
+
+    // Broken Code:
+    /*
+    7/0;
+    */
+    
+    // Still Broken but given better information on how to solve:
+    /*
+    int value = 7 / 0 ;
+    std::cout << "value : " << value << std::endl;
+    */
+    
+   
+
+    return 0;
+
+}
