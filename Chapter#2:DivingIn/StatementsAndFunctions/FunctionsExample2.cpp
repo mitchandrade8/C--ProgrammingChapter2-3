ce352794794ee2f8244933dc6a8e64fd5f271631
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-06 15:59:54 -0700

    --Completed the 2nd Example: Functions

--- Chapter#2:DivingIn/StatementsAndFunctions/FunctionsExample2.cpp
+++ Chapter#2:DivingIn/StatementsAndFunctions/FunctionsExample2.cpp
@@ -2,9 +2,22 @@
 
 #include <iostream>
 
-int main() {
+int addNumbers( int first_param, int second_param) {
 
-    int first_number {3}; // Statement
+    int result = first_param + second_param;
+    return result;
+}
+
+int main(int argc, char **argv) {
+
+    int first_number = 13; // Statement
+    int second_number = 7;
+
+    std::cout << "First number : " << first_number << std::endl;
+    std::cout << "Second number : " << second_number << std::endl;
+
+    int sum = first_number + second_number;
+    std::cout << "Sum : " << sum << std::endl;
 
     return 0;
     
