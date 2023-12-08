2f056cfdc30aefcc9ffa3af0cb40862152cd2e56
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-08 12:29:09 -0700

    -Reading Data with Spaces in C++

---
+++ Chapter#2:DivingIn/InputsAndOutputs/ReadingDataWithSpacesExample.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+
+int main() {
+
+    int age;
+    std::string full_name;
+
+    std::cout << "Please type in your full name : " << std::endl;
+    std::getline(std::cin, full_name);
+
+    std::cout << "Type in your age : " << std::endl;
+    std::cin >> age;
+    std::cout << "Hello " << full_name << "! You are " << age << "years old." << std::endl;
+
+    return 0;
+
+}
