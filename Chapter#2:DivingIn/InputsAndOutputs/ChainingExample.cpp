80c7b0fd0f54b64795b3ce25b9b442e50b4931cc
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-07 19:14:01 -0700

    chaining example in C++ with Cin

---
+++ Chapter#2:DivingIn/InputsAndOutputs/ChainingExample.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+int main() {
+
+    int age;
+    std::string name;
+
+    std::cout << "Please type in your Last name and age, sepertated by spaces : " << std::endl;
+
+    std::cin >> name >> age ;
+
+    std:: cout << "Hello " << name << "! You are " << age << " years old." << std::endl;
+
+    return 0;
+
+}
